550998c6401306d74681b6fc1b98ba219d91c25d
Graeme Stuart <ggstuart@gmail.com>
2021-03-02 15:57:29 +0000

    add title to article as h2

--- scripts.js
+++ scripts.js
@@ -3,21 +3,27 @@
 const articleData = [
 	{
 		"imgId": 1032,
+		"title": "crater",
 	},
 	{
 		"imgId": 103,
+		"title": "feet",
 	},
 	{
 		"imgId": 1039,
+		"title": "waterfall",
 	}
 ];
 
 function buildArticleFromData(data) {
 	const article = document.createElement("article");
 	const img = document.createElement('img');
+	const h2 = document.createElement('h2');
+	h2.textContent = data["title"];
 	img.alt = `picsum image ${data["imgId"]}`;
 	img.src = `https://picsum.photos/id/${data["imgId"]}/150`;
 	article.appendChild(img);
+	article.appendChild(h2);
 	return article;
 }
 
